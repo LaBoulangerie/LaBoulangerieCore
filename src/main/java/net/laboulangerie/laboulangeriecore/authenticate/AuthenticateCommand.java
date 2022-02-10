705e3154a62fd5ccbae2300d1cb417c8e29fa326
GroobleDierne <grooble.dierne@gmail.com>
2022-02-10 22:59:49 +0100

    Added autocompletion

--- src/main/java/net/laboulangerie/laboulangeriecore/authenticate/AuthenticateCommand.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/authenticate/AuthenticateCommand.java
@@ -14,6 +14,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -21,7 +22,7 @@ import org.jetbrains.annotations.Nullable;
 
 import net.kyori.adventure.text.Component;
 
-public class AuthenticateCommand implements @Nullable CommandExecutor {
+public class AuthenticateCommand implements @Nullable CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String alias,
@@ -77,4 +78,9 @@ public class AuthenticateCommand implements @Nullable CommandExecutor {
         return true;
     }
 
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 1) return Arrays.asList("player", "town", "nation");
+        return Arrays.asList("");
+    }
 }
