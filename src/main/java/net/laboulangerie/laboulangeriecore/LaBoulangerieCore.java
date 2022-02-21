2fbd4f4eca0234e5f4277bd56579612533faa328
GroobleDierne <grooble.dierne@gmail.com>
2022-02-21 23:49:04 +0100

    🔃️Added LoreUpdater

--- src/main/java/net/laboulangerie/laboulangeriecore/LaBoulangerieCore.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/LaBoulangerieCore.java
@@ -3,6 +3,7 @@ package net.laboulangerie.laboulangeriecore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.laboulangerie.laboulangeriecore.authenticate.AuthenticateCommand;
+import net.laboulangerie.laboulangeriecore.authenticate.LoreUpdater;
 
 public class LaBoulangerieCore extends JavaPlugin {
     public static LaBoulangerieCore PLUGIN;
@@ -10,6 +11,8 @@ public class LaBoulangerieCore extends JavaPlugin {
     public void onEnable() {
         getCommand("authenticate").setExecutor(new AuthenticateCommand());
         LaBoulangerieCore.PLUGIN = this;
+        getServer().getPluginManager().registerEvents(new LoreUpdater(), this);
+
         getLogger().info("Enabled Successfully");
     }
 
---
+++ src/main/java/net/laboulangerie/laboulangeriecore/authenticate/LoreUpdater.java
@@ -0,0 +1,44 @@
+package net.laboulangerie.laboulangeriecore.authenticate;
+
+import com.palmergames.bukkit.towny.TownyUniverse;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+
+public class LoreUpdater implements Listener {
+    @EventHandler
+    public void onPickupEvent(EntityPickupItemEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+
+        Authenticable authenticable = new Authenticable(event.getItem().getItemStack());
+
+        if (!authenticable.isAuthenticated()) return;
+
+        String newName = null;
+
+        switch (authenticable.getAuthorityType()) {
+            case PLAYER:
+                newName = Bukkit.getOfflinePlayer(authenticable.getAuthorityUUID()).getName();
+                break;
+            case TOWN:
+                // If the town no longer exists we leave the lore as is
+                if (!TownyUniverse.getInstance().hasTown(authenticable.getAuthorityUUID())) return;
+                newName = TownyUniverse.getInstance().getTown(authenticable.getAuthorityUUID()).getName();
+                break;
+            case NATION:
+                // If the nation no longer exists we leave the lore as is
+                if (!TownyUniverse.getInstance().hasNation(authenticable.getAuthorityUUID())) return;
+                newName = TownyUniverse.getInstance().getNation(authenticable.getAuthorityUUID()).getName();
+                break;
+            default:
+                break;
+        }
+
+        if (newName.equals(authenticable.getAuthorityName())) return;
+
+        authenticable.updateAuthorityName(newName);
+    }
+}
