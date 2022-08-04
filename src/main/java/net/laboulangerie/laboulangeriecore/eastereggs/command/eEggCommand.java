20101bcf1ec3d6d90b1fd9bdd4e9c1a98a2fc79d
GroobleDierne <grooble.dierne@gmail.com>
2022-08-04 16:27:18 +0200

    Fix eEggCommand

--- src/main/java/net/laboulangerie/laboulangeriecore/eastereggs/command/eEggCommand.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/eastereggs/command/eEggCommand.java
@@ -1,20 +1,23 @@
 package net.laboulangerie.laboulangeriecore.eastereggs.command;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import net.laboulangerie.laboulangeriecore.LaBoulangerieCore;
 import net.laboulangerie.laboulangeriecore.eastereggs.Utils.eEggFileUtil;
 import net.laboulangerie.laboulangeriecore.eastereggs.Utils.eEggUtil;
 
-public class eEggCommand implements CommandExecutor {
+public class eEggCommand implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
 
@@ -28,8 +31,8 @@ public class eEggCommand implements CommandExecutor {
             p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.utilization"));
             return true;
         }
-        if(args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("add")){
-            if(p.hasPermission("eastereggs.add")) {
+        if (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("add")){
+            if (!p.hasPermission("laboulangeriecore.admin")) {
                 p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.permission"));
                 return true;
             }
@@ -54,14 +57,14 @@ public class eEggCommand implements CommandExecutor {
             return true;
         }
 
-        if(args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("delete")) {
-            if(!p.hasPermission("eastereggs.remove")) {
+        if (args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("delete")) {
+            if (!p.hasPermission("laboulangeriecore.admin")) {
                 p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.permission"));
                 return true;
             }
 
             Block block = p.getTargetBlockExact(5);
-            if(block.getType() != Material.PLAYER_HEAD && block.getType() == Material.PLAYER_WALL_HEAD) {
+            if (block.getType() != Material.PLAYER_HEAD && block.getType() == Material.PLAYER_WALL_HEAD) {
                 p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.not-head"));
                 return true;
             }
@@ -85,17 +88,27 @@ public class eEggCommand implements CommandExecutor {
             return true;
             
         }
-        if(args[0].equalsIgnoreCase("help")) {
-            if(p.hasPermission("eastereggs.help")) {
+        if (args[0].equalsIgnoreCase("help")) {
+            if (p.hasPermission("laboulangeriecore.admin")) {
                 List<String> list = LaBoulangerieCore.PLUGIN.getConfig().getStringList("eastereggs.messages.help");
                 for (int i = 0; i < list.size(); i++) {
                     p.sendMessage(list.get(i));
                 }
-            }else p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.permission"));
+            } else p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.permission"));
             return true;
         }
 
         p.sendMessage(LaBoulangerieCore.PLUGIN.getConfig().getString("eastereggs.messages.utilization"));
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String alias, @NotNull String[] args) {
+        if (args.length != 1) return null;
+
+        if (sender.hasPermission("laboulangeriecore.admin")) {
+            return Arrays.asList("add", "help", "remove");
+        }
+        return null;
     }
 }
