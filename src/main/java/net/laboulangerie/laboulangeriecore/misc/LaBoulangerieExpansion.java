833120b99d45c51f25e95f4ea6f4f3711e3a4e6b
Matteo Planchet <matteo.planchet@gmail.com>
2023-04-09 01:19:39 +0200

    Added placeholders for Gaiartos Date

--- src/main/java/net/laboulangerie/laboulangeriecore/misc/LaBoulangerieExpansion.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/misc/LaBoulangerieExpansion.java
@@ -1,8 +1,10 @@
 package net.laboulangerie.laboulangeriecore.misc;
 
+import java.time.LocalDate;
 import org.bukkit.entity.Player;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.laboulangerie.laboulangeriecore.core.GaiartosDate;
 
 /**
  * LaBoulangerieExpansion
@@ -30,6 +32,7 @@ public class LaBoulangerieExpansion extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player player, String params) {
+
         if (params.startsWith("key_")) {
             String name = params.split("_")[1];
             char key = ' ';
@@ -68,6 +71,23 @@ public class LaBoulangerieExpansion extends PlaceholderExpansion {
                     return null;
             }
             return "<font:bread_dough:controls>" + key + "</font>";
+        }
+
+        if (params.startsWith("date_")) {
+            String option = params.split("_")[1];
+            LocalDate now = LocalDate.now();
+            GaiartosDate gNow = new GaiartosDate(now);
+
+            switch (option) {
+                case "month":
+                    return gNow.getMonth();
+                case "year":
+                    return ((Integer) gNow.getYear()).toString();
+                case "full":
+                    return String.format("%d %s An %d", now.getDayOfMonth(), gNow.getMonth(), gNow.getYear());
+                default:
+                    return null;
+            }
         }
         return null;
     }
