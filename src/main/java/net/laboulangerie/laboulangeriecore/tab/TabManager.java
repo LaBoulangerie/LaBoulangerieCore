9f6e8f0a3e4e510c84160743b2a83e7f39a60bc1
GroobleDierne <grooble.dierne@gmail.com>
2023-05-17 23:31:36 +0200

    Tab ordering seems working Plus disabled NameTagManager

--- src/main/java/net/laboulangerie/laboulangeriecore/tab/TabManager.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/tab/TabManager.java
@@ -1,24 +1,29 @@
 package net.laboulangerie.laboulangeriecore.tab;
 
-import net.laboulangerie.laboulangeriecore.LaBoulangerieCore;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.model.group.Group;
-import net.luckperms.api.model.user.User;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-import javax.annotation.Nonnull;
-import java.util.Set;
+import net.laboulangerie.laboulangeriecore.LaBoulangerieCore;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
 
 public class TabManager {
-
     private final LuckPerms lpApi;
-
     private final TabRenderer tabRenderer;
+    private HashMap<Group, Short> groupsMap = new HashMap<>();
 
     public TabManager() {
         this.lpApi = LuckPermsProvider.get();
@@ -32,9 +37,17 @@ public class TabManager {
         final Set<Group> groups = lpApi.getGroupManager().getLoadedGroups();
         final Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
 
-        for (final Group g : groups) {
-            final String teamName = g.getName();
-            if (board.getTeam(teamName) != null) continue;
+        List<Group> sortedGroups = new ArrayList<>();
+        sortedGroups.addAll(groups);
+        Collections.sort(sortedGroups, (a, b) -> (b.getWeight().isPresent() ? b.getWeight().getAsInt() : 0)
+                - (a.getWeight().isPresent() ? a.getWeight().getAsInt() : 0));
+
+        for (short i = 0; i < sortedGroups.size(); i++) {
+            groupsMap.put(sortedGroups.get(i), i);
+            final String teamName = i + sortedGroups.get(i).getName();
+            if (board.getTeam(teamName) != null)
+                continue;
+
             Team team = board.registerNewTeam(teamName);
             team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
             team.setCanSeeFriendlyInvisibles(false);
@@ -43,12 +56,14 @@ public class TabManager {
 
     public Team getTeam(@Nonnull Player player) {
         final User user = lpApi.getUserManager().getUser(player.getUniqueId());
-        if (user == null) return null;
+        if (user == null)
+            return null;
 
         final Group group = lpApi.getGroupManager().getGroup(user.getPrimaryGroup());
-        if (group == null) return null;
+        if (group == null)
+            return null;
 
-        final String teamName = group.getName();
+        final String teamName = (groupsMap.containsKey(group) ? groupsMap.get(group) : "" ) + group.getName();
         final Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
         return board.getTeam(teamName);
     }
