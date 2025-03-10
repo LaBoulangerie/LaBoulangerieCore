609a3e8c30f04bf0fbf115a331d920aba7485785
GroleDev <73291571+GroleDev@users.noreply.github.com>
2025-03-10 12:32:21 +0100

    Hotfix

--- src/main/java/net/laboulangerie/laboulangeriecore/moreroleplay/wRollCommand.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/moreroleplay/wRollCommand.java
@@ -12,40 +12,47 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.laboulangerie.laboulangeriecore.core.UsersData;
 
-public class MoreRolePlayCommand implements CommandExecutor {
+public class RollCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) { // Vérification de l'existence du joueur
             sender.sendMessage("§4You must be in game to use this command!");
             return true;
         }
 
-        if (args != null) {
+        int max = 100;
+        int result;
+
+        if (args.length == 0) { // Détermination du maximum (si renseigné)
             int result = random.nextInt(100) + 1;
-        } else if {
+        } else {
             try {
-                int max = Integer.parseInt(args[0]);
+                max = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
                 sender.sendMessage("§4Merci d'indiquer un nombre entier en tant que maximum.");
+                return true;
             }
 
             if (max != 0){
-                int result = random.nextInt(max) + 1;
+                result = random.nextInt(max) + 1;
             } else {
                 sender.sendMessage("§40 n'est pas un maximum valide.");
                 return true;
             }
         }
 
-        for (Player target : player.getWorld().getPlayers()) {
+        // Envoie du résultat aux joueurs
+        string message;
+
+        for (Player target : Bukkit.getOnlinePlayers()) {
             if(result == max)
-                target.sendMessage("&2&lLes dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max +", c'est une réussite critique !");
+                message = "Les dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max + ", c'est une réussite critique !";
             else if(result == 1)
-                target.sendMessage("&4&lLes dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max +", c'est un échec critique !");
+                message = "Les dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max + ", c'est un échec critique !";
             else
-                target.sendMessage("&e&lLes dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max +".");
-            
-            target.playSound(target.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
+                message = "Les dieux ont jeté les dés de votre destin, et ont obtenu " + result + "/" + max + ".";
+
+            target.sendMessage(Component.text(message, result == max ? NamedTextColor.DARK_GREEN : result == 1 ? NamedTextColor.DARK_RED : NamedTextColor.YELLOW));
         }
         
         return true;
