f3a17a185bb9781a25b55605a59fbd618ca0f64c
GroleDev <73291571+GroleDev@users.noreply.github.com>
2025-03-10 12:32:21 +0100

    Hotfix

--- src/main/java/net/laboulangerie/laboulangeriecore/moreroleplay/RollCommand.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/moreroleplay/RollCommand.java
@@ -12,42 +12,52 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.laboulangerie.laboulangeriecore.core.UsersData;
 
-public class MoreRolePlayCommand implements CommandExecutor {
+public class RollCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) { // Vérification de l'existence du joueur
             sender.sendMessage("§4You must be in game to use this command!");
             return true;
         }
 
-        Location playerLocation = player.getLocation();
-        int radius = plugin.getConfig().getInt("roll-radius", 10);
+        int max = 100;
+        int result;
+        string message;
 
-        if (args != null) {
+        if (args.length == 0) { // Détermination du maximum (si renseigné)
             int result = random.nextInt(100) + 1;
-        } else if {
+        } else {
             try {
-                int max = Integer.parseInt(args[0]);
+                max = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
-                sender.sendMessage("§4Merci d'indiquer un nombre entier en tant que maximum.");
+                sender.sendMessage(Component.text("Merci d'indiquer un nombre entier en tant que maximum.", NamedTextColor.DARK_RED));
+                return true;
             }
 
             if (max != 0){
-                int result = random.nextInt(max) + 1;
+                result = random.nextInt(max) + 1;
             } else {
-                sender.sendMessage("§40 n'est pas un maximum valide.");
+                sender.sendMessage(Component.text("0 n'est pas un maximum valide.", NamedTextColor.DARK_RED));
                 return true;
             }
         }
 
+        // Envoie du résultat aux joueurs proches
+        Location playerLocation = sender.getLocation();
+        int radius = LaBoulangerieCore.PLUGIN.getConfig().getInt("roll-radius", 10);
+
         for (Player target : player.getWorld().getPlayers()) {
             if (target.getLocation().distance(playerLocation) <= radius) {
                 if(result == max)
-                    target.sendMessage("&2" + sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max +", c'est une réussite critique !");
+                    message = sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max + ", c'est une réussite critique !";
                 else if(result == 1)
-                    target.sendMessage("&4" + sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max +", c'est un échec critique !");
+                    message = sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max + ", c'est un échec critique !";
                 else
-                    target.sendMessage("&e" + sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max +".");
+                    message = sender.displayName() + " a jeté les dés de son destin, et a obtenu " + result + "/" + max + ".";
+
+                target.sendMessage(Component.text(message)
+                    .color(result == max ? NamedTextColor.DARK_GREEN : result == 1 ? NamedTextColor.DARK_RED : NamedTextColor.YELLOW)
+                    .decorate(TextDecoration.BOLD));
             }
         }
         
