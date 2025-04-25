fed4a85fc2317b5b3737d1d35d2d5b50ff615be0
GroleDev <73291571+GroleDev@users.noreply.github.com>
2025-04-25 20:00:07 +0200

    Ajout LandsUtils

--- src/main/java/net/laboulangerie/laboulangeriecore/lands/LandsListener.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/lands/LandsListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 
 import me.angeschossen.lands.api.LandsIntegration;
 import me.angeschossen.lands.api.events.LandInvitePlayerEvent;
+import me.angeschossen.lands.api.events.LandTrustPlayerEvent;
 import me.angeschossen.lands.api.land.Area;
 import me.angeschossen.lands.api.land.Land;
 
@@ -18,45 +19,39 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.laboulangerie.laboulangeriecore.LaBoulangerieCore;
 
 public class LandsListener implements Listener {
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     private void onInviteInArea(LandInvitePlayerEvent event) {
-        LandsIntegration apiLands = LandsIntegration.of(LaBoulangerieCore.PLUGIN);
-        Area area = event.getArea();
         Player sender = Bukkit.getPlayer(event.getPlayerUUID());
         OfflinePlayer target = Bukkit.getOfflinePlayer(event.getTargetUUID());
+        Area area = event.getArea();
 
         if(area.isDefault()){
-            for(Land land : apiLands.getLands()) {
-                if(land.getDefaultArea().getTrustedPlayers().contains(target.getUniqueId())){
-                    sender.sendMessage(
-                        Component.text("[").color(NamedTextColor.DARK_GRAY)
-                        .append(Component.text("Lands").color(NamedTextColor.DARK_GREEN))
-                        .append(Component.text("] ").color(NamedTextColor.DARK_GRAY))
-                        .append(Component.text(event.getLand().getColorName()).color(NamedTextColor.DARK_GREEN))
-                        .append(Component.text(" | ").color(NamedTextColor.DARK_GRAY))
-                        .append(Component.text(target.getName()).color(NamedTextColor.DARK_AQUA))
-                        .append(Component.text(" est déjà un habitant de ").color(NamedTextColor.GRAY))
-                        .append(Component.text(land.getColorName()).color(NamedTextColor.GREEN))
-                        .append(Component.text(", vous ne pouvez pas l'inviter dans cette zone !").color(NamedTextColor.GRAY))
-                    );
+            LandsIntegration apiLands = LandsIntegration.of(LaBoulangerieCore.PLUGIN);
+            Land targetMainLand = LandsUtils.getPlayerMainLandOrNull(apiLands, target);
 
-                    event.setCancelled(true);
-                }
+            if(targetMainLand != null) { // Annulation de l'évènement
+                sender.sendMessage(
+                    Component.text("[").color(NamedTextColor.DARK_GRAY)
+                    .append(Component.text("Lands").color(NamedTextColor.DARK_GREEN))
+                    .append(Component.text("] ").color(NamedTextColor.DARK_GRAY))
+                    .append(Component.text(event.getLand().getColorName()).color(NamedTextColor.DARK_GREEN))
+                    .append(Component.text(" | ").color(NamedTextColor.DARK_GRAY))
+                    .append(Component.text(target.getName()).color(NamedTextColor.DARK_AQUA))
+                    .append(Component.text(" est déjà un habitant de ").color(NamedTextColor.GRAY))
+                    .append(Component.text(targetMainLand.getColorName()).color(NamedTextColor.GREEN))
+                    .append(Component.text(", vous ne pouvez pas l'inviter dans cette zone !").color(NamedTextColor.GRAY))
+                );
+
+                event.setCancelled(true);
             }
-        } else {
-            area.trustPlayer(target.getUniqueId());
-
-            sender.sendMessage(
-                Component.text("[").color(NamedTextColor.DARK_GRAY)
-                .append(Component.text("Lands").color(NamedTextColor.DARK_GREEN))
-                .append(Component.text("] ").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text(event.getLand().getColorName()).color(NamedTextColor.DARK_GREEN))
-                .append(Component.text(" | ").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text(target.getName()).color(NamedTextColor.DARK_AQUA))
-                .append(Component.text(" a été ajouté à la zone " + area.getName() + ".").color(NamedTextColor.GRAY))
-            );
-
-            event.setCancelled(true);
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    private void onJoinArea(LandTrustPlayerEvent event) {
+        Land targetMainLand = LandsUtils.getPlayerMainLandOrNull(LandsIntegration.of(LaBoulangerieCore.PLUGIN), Bukkit.getOfflinePlayer(event.getTargetUUID()));
+    
+        // Annule l'évènement si l'area est une zone default et le joueur est déjà membre d'une zone default
+        if(event.getArea().isDefault() && targetMainLand != null) event.getLand().untrustPlayer(event.getPlayerUUID());
     }
 }
