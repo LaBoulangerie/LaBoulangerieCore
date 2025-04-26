2b6ce91d658c53f07b6f90d1965f8adede826186
GroleDev <73291571+GroleDev@users.noreply.github.com>
2025-04-26 11:53:23 +0200

    Accolade

--- src/main/java/net/laboulangerie/laboulangeriecore/lands/LandsUtils.java
+++ src/main/java/net/laboulangerie/laboulangeriecore/lands/LandsUtils.java
@@ -7,7 +7,13 @@ import me.angeschossen.lands.api.land.Land;
 
 public class LandsUtils {
     public static Land getPlayerMainLandOrNull (LandsIntegration apiLands, OfflinePlayer target) {
-        for(Land land : apiLands.getLands()) if(land.getDefaultArea().getTrustedPlayers().contains(target.getUniqueId())) return land;
+        for(Land land : apiLands.getLands())
+        {
+            if(land.getDefaultArea().getTrustedPlayers().contains(target.getUniqueId())) 
+            {
+                return land;
+            }
+        }
         
         return null;
     }
